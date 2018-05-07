1e9b16f826d8f937643317410ac6a2a363b5ed0d
XanderXu1998 <xanderxu1998@gmail.com>
2018-05-08 00:39:29 +0800

    first

---
+++ catch.py
@@ -0,0 +1,19 @@
+import cv2
+import time
+cap = cv2.VideoCapture(0)
+
+cnt = 0
+while(cap.isOpened()):
+  time.sleep(3)
+  ret,frame = cap.read()
+  if ret:
+    cv2.imshow('frame',frame)
+    cv2.imwrite('xu{}.jpg'.format(cnt),frame)
+    cnt += 1
+    if cnt > 5:
+      break
+  else:
+    break
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+    
---
+++ unitTest.py
@@ -0,0 +1,4 @@
+import face_rec as fr
+
+tls = fr.detect('xu0.jpg')
+fr.setuserid(tls[0], ID = '001')
